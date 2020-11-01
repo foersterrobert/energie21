9199ddbf8758b1557c8123c16488429cc65e97cc
robertfoerster <foersteroberthh@gmail.com>
2020-11-01 17:07:55 +0100

    toggle nav

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 jQuery(".intro__text").fitText(1.2);
+
+$('.navigation__toggle').click(function() {
+  
+    $('.navigation__nav').toggleClass('navigation__nav--open', 500);
+    $(this).toggleClass('open');
+    
+  })
