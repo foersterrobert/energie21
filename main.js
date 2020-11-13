1ba3f919a38226fd100c1703109485786d7dbc05
robertfoerster <foersteroberthh@gmail.com>
2020-11-13 11:28:40 +0100

    sticky nav

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ $('.navigation__toggle').click(function() {
     $(this).toggleClass('open');
     
   })
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
