9b69c9c53039ba41dbbe303329c1b4ed7fe36a69
robertfoerster <foersteroberthh@gmail.com>
2020-11-13 15:43:25 +0100

    nav open close

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 jQuery(".intro__text").fitText(1.8);
 
 $('.navigation__toggle').click(function() {
-    $(document.getElementById("navigation").style.height="120px");
     $('.navigation__nav').toggleClass('navigation__nav--open', 500);
     $(this).toggleClass('open');
+    $('.navigation__nav').slideToggle();
+
     
   })
