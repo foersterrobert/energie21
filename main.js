c72f502caa2a0fcee9eeef60b0635abadbfccc99
robertfoerster <foersteroberthh@gmail.com>
2020-11-13 11:35:03 +0100

    nav fix no js now

--- main.js
+++ main.js
@@ -6,18 +6,3 @@ $('.navigation__toggle').click(function() {
     $(this).toggleClass('open');
     
   })
-
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
