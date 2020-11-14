600756d8d6716bbf106cd2ec8923cda883e093c5
robertfoerster <foersteroberthh@gmail.com>
2020-11-14 10:46:29 +0100

    new homepage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-jQuery(".intro__text").fitText(1.8);
+jQuery(".strom__intro__text").fitText(1.8);
 
 $('.navigation__toggle').click(function() {
     $('.navigation__nav').toggleClass('navigation__nav--open', 500);
-    $(this).toggleClass('open');
     $('.navigation__nav').slideToggle();
+    $(this).toggleClass('open');
   })
