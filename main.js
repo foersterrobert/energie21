b950773e06879467c3d554a0eb1d5403b7dd98ed
robertfoerster <foersteroberthh@gmail.com>
2020-11-14 11:57:40 +0100

    style fixes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-jQuery(".strom__intro__text").fitText(1.8);
+jQuery(".strom__intro__text").fitText(2);
 
 $('.navigation__toggle').click(function() {
     $('.navigation__nav').toggleClass('navigation__nav--open', 500);
