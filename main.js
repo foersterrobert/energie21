770731bd3e141b5c81f1ed5f3f65d6bc146c99cc
robertfoerster <foersteroberthh@gmail.com>
2020-11-22 17:30:47 +0100

    title fixes

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-jQuery(".intro__text").fitText(2.4);
-
-jQuery(".intro__text--2").fitText(3);
-
-jQuery(".intro__text--card").fitText(1.8);
-
-jQuery(".intro__info__card h4").fitText(1.3);
-
-jQuery(".intro__title").fitText(2.5);
-
-
 $('.navigation__toggle').click(function() {
     $('.navigation__nav').toggleClass('navigation__nav--open', 500);
     $('.navigation__nav').slideToggle();
